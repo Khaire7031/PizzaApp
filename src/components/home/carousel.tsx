ada51d2ecc9949fc9472ffd89d44909a4964d8aa
Pranav Khaire <pranavkhaire28@gmail.com>
2025-01-26 11:20:39 +0530

    Update PizzaData

--- src/components/home/carousel.tsx
+++ src/components/home/carousel.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 
 export default function Carousel() {
     const [currentIndex, setCurrentIndex] = useState(0);
